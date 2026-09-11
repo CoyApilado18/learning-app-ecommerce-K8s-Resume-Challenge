ad70a6a21ab47b655e723769989e0b4b1a9eb60b
emmanuel <apilado.emmanuel18@gmail.com>
2026-09-11 14:06:56 -0500

    Fixed fqdn in readyz.php

--- readyz.php
+++ readyz.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = @new mysqli("mysql-service.ecomwebapp.svc.cluster.local", "ecomuser", "ecompassword", "ecomdb");
+$mysqli = @new mysqli("mysql-service.cicd-test.svc.cluster.local", "ecomuser", "ecompassword", "ecomdb");
 
 if ($mysqli->connect_error) {
     http_response_code(503);
