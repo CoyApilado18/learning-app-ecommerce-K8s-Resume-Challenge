b7a66270e7f2578336fad80bde6b975029a0bdf9
emmanuel <apilado.emmanuel18@gmail.com>
2026-09-12 21:44:16 -0500

    Use environment variables in readiness check

--- readyz.php
+++ readyz.php
@@ -1,5 +1,9 @@
 <?php
-$mysqli = @new mysqli("mysql-service.cicd-test.svc.cluster.local", "ecomuser", "ecompassword", "ecomdb");
+$dbHost = getenv('DB_HOST') ?: 'mysql-service';
+$dbUser = getenv('DB_USER') ?: 'ecomuser';
+$dbPassword = getenv('DB_PASSWORD') ?: 'ecompassword';
+$dbName = getenv('DB_NAME') ?: 'ecomdb';
+$mysqli = @new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
 
 if ($mysqli->connect_error) {
     http_response_code(503);
